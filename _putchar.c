72c918518fb47a99e53237ee71e2227892cda00c
Lamia Hamdi <lamia.hamdi@univ-constantine2.dz>
2023-07-22 11:05:15 -0700

    Implement print_char function for %c format specifier

---
+++ _putchar.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * _putchar - function that prints a character
+ * @ch: the character to print
+ *
+ * Return: int
+ **/
+int _putchar(char ch)
+{
+	return (putchar(ch));
+}
+
