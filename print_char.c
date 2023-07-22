6478c6ff52a2ba02513ff173dd5f64c946103ab3
Lamia Hamdi <lamia.hamdi@univ-constantine2.dz>
2023-07-22 11:02:55 -0700

    Implement print_char function for %c format specifier

---
+++ print_char.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_char - function that outputs a character
+ * @list: argument list containing a character to be printed.
+ *
+ * Return: 1 (Successful)
+ **/
+int print_char (va_list list)
+{
+	_putchar(va_arg(list, int));
+	return (1);
+}
