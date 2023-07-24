398c432102454385298ec3e8a90d295c97939479
Lamia Hamdi <lamia.hamdi@univ-constantine2.dz>
2023-07-23 21:23:34 -0700

    Implement print_decimal_int function for %d format specifier

--- format_controller.c
+++ format_controller.c
@@ -60,11 +60,7 @@ int j = 0;
 int len_printed_chars = 0;
 int nb_specs = 0;
 /*declare array of struct format*/
-format print_specs[] = {
-	{'c', print_char},
-	{'s', print_str},
-	{'d', print_decimal}
-};
+format print_specs[] = {{'c', print_char}, {'s', print_str}};
 /*loop throught array print_specs*/
 nb_specs = sizeof(print_specs) / sizeof(print_specs[0]);
 for (j = 0; j < nb_specs; j++)
