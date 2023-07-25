2d42d96749e7670776ea9e289059ac5ad07323f9
Lamia Hamdi <lamia.hamdi@univ-constantine2.dz>
2023-07-25 16:29:32 -0700

    modify print_binary function

--- print_binary.c
+++ print_binary.c
@@ -9,39 +9,33 @@
  **/
 int print_binary(va_list list)
 {
-	unsigned int num = va_arg(list, unsigned int);
-	unsigned int *buffer_int;
-	int i = 0, index = 0;
-	unsigned int temp_num = num;
+	int num = va_arg(list, int);
+	int i = 0;
+	unsigned int mask;
+	int binary_digits = sizeof(num) * 8;
+	int leading_zero = 1;
 
+	if (num < 0)
+		return (-1);
 	if (num < 2)
 	{
 		_putchar('0' + num);
 		_putchar('\n');
 		return (1);
 	}
-	while (temp_num > 0)
+
+	for (i = binary_digits - 1; i >= 0; i--)
 	{
-		i++;
-		temp_num = temp_num / 2;
+		mask = 1 << i;
+
+		if (num & mask)
+		{
+			leading_zero = 0;
+			_putchar('1');
+		}
+		else if (!leading_zero)
+			_putchar('0');
 	}
-
-	buffer_int = (unsigned int *)malloc(sizeof(unsigned int) * i);
-
-	if (!buffer_int)
-		return (-1);
-
-	while (num > 0)
-	{
-		buffer_int[index++] = num % 2;
-		num = num / 2;
-	}
-
-	for (i = index - 1; i >= 0; i--)
-		_putchar('0' + buffer_int[i]);
-
-	_putchar('\n');
-	free(buffer_int);
 	return (1);
 }
 
