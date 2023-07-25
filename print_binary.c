74e38865424f8305efe9c521b8227184057a0540
Lamia Hamdi <lamia.hamdi@univ-constantine2.dz>
2023-07-25 09:31:42 -0700

    fix bugs

--- print_binary.c
+++ print_binary.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdlib.h>
+
 /**
  * print_binary - function that converts a number into binary
  * @list: argument list containing a character to be printed.
@@ -7,18 +9,27 @@
  **/
 int print_binary(va_list list)
 {
-	int num = va_arg(list, int);
-	int buffer_int[32];
-	int i, index = 0;
+	unsigned int num = va_arg(list, unsigned int);
+	unsigned int *buffer_int;
+	int i = 0, index = 0;
+	unsigned int temp_num = num;
 
-	if (num < 0)
-		return (-1);
 	if (num < 2)
 	{
 		_putchar('0' + num);
 		_putchar('\n');
 		return (1);
 	}
+	while (temp_num > 0)
+	{
+		i++;
+		temp_num = temp_num / 2;
+	}
+
+	buffer_int = (unsigned int *)malloc(sizeof(unsigned int) * i);
+
+	if (!buffer_int)
+		return (-1);
 
 	while (num > 0)
 	{
@@ -30,6 +41,7 @@ int print_binary(va_list list)
 		_putchar('0' + buffer_int[i]);
 
 	_putchar('\n');
+	free(buffer_int);
 	return (1);
 }
 
