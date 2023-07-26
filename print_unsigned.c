3c1ce0598d72d45fe1b23a2d84027a3f746388f5
Lamia Hamdi <lamia.hamdi@univ-constantine2.dz>
2023-07-25 17:08:31 -0700

    modify print_unsigned function

--- print_unsigned.c
+++ print_unsigned.c
@@ -2,44 +2,6 @@
 #include <stdlib.h>
 
 /**
- * nb_digits - calculate number of digites of number
- * @n: number
- * Return: int
- **/
-int nb_digits(int n)
-{
-	if (n == 0)
-		return (0);
-	return (1 + nb_digits(n / 10));
-}
-/**
- *  nb_to_str - Convert an unsigned integer to a string representation.
- *  @n: unsigned number
- *  Return: pointer to char
- **/
-char *nb_to_str(unsigned int n)
-{
-	int size, rem;
-	int idx = 0;
-	char *ptr;
-
-	size = nb_digits(n);
-	ptr = (char *)malloc(size + 1);
-	if (ptr == NULL)
-		return (NULL);
-	ptr[size] = '\0';
-	idx = size - 1;
-	while (idx >= 0)
-	{
-		rem = n % 10;
-		n = n / 10;
-		ptr[idx] = rem + '0';
-		idx--;
-	}
-	return (ptr);
-}
-
-/**
  * print_unsigned - function that prints only unsigned numbers
  * @list: argument list containing a character to be printed.
  *
@@ -47,18 +9,23 @@ char *nb_to_str(unsigned int n)
  **/
 int print_unsigned(va_list list)
 {
-	unsigned int nb = va_arg(list, unsigned int);
-	char *ptr_nb = nb_to_str(nb);
-	int len = 0;
+	unsigned int num = va_arg(list, unsigned int);
+	unsigned int divisor = 1;
+	unsigned int nb = num;
+	int digit;
 
-	if (ptr_nb == NULL)
-		return (0);
-	while (ptr_nb[len] != '\0')
+	while (num >= 10)
 	{
-		_putchar(ptr_nb[len]);
-		len++;
+		divisor *= 10;
+		num /= 10;
 	}
-	free(ptr_nb);
-	return (len);
-}
 
+	while (divisor > 0)
+	{
+		digit = (nb / divisor) % 10;
+		_putchar('0' + digit);
+		divisor /= 10;
+	}
+
+	return (1);
+}
