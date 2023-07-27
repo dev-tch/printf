7de6320331d40a29af934516caae88a0bd1cea53
dev-tch <tijeni.chaabeni@gmail.com>
2023-07-27 00:57:07 +0000

    fix len

--- print_binary.c
+++ print_binary.c
@@ -9,33 +9,36 @@
  **/
 int print_binary(va_list list)
 {
-	int num = va_arg(list, int);
-	int i = 0;
-	unsigned int mask;
-	int binary_digits = sizeof(num) * 8;
-	int leading_zero = 1;
-
-	if (num < 0)
-		return (-1);
-	if (num < 2)
-	{
-		_putchar('0' + num);
-		_putchar('\n');
-		return (1);
-	}
-
-	for (i = binary_digits - 1; i >= 0; i--)
-	{
-		mask = 1 << i;
-
-		if (num & mask)
-		{
-			leading_zero = 0;
-			_putchar('1');
-		}
-		else if (!leading_zero)
-			_putchar('0');
-	}
-	return (1);
+int num = va_arg(list, int);
+int i = 0;
+/*fix_1 len*/
+int len = 0;
+unsigned int mask;
+int binary_digits = sizeof(num) * 8;
+int leading_zero = 1;
+if (num < 0)
+return (-1);
+if (num < 2)
+{
+_putchar('0' + num);
+_putchar('\n');
+return (1);
+}
+for (i = binary_digits - 1; i >= 0; i--)
+{
+mask = 1 << i;
+if (num & mask)
+{
+leading_zero = 0;
+_putchar('1');
+len++;
+}
+else if (!leading_zero)
+{
+_putchar('0');
+len++;
+}
+}
+return (len);
 }
 
