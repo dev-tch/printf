8434ba3d9ded03868d0bf113ebc2da783502381e
dev-tch <tijeni.chaabeni@gmail.com>
2023-07-30 23:37:07 +0000

    fix print_octal

--- print_octal.c
+++ print_octal.c
@@ -2,35 +2,45 @@
 /**
  * print_octal - function that outputs a number in octal format
  * @list: argument list containing a character to be printed.
- *
- * Return: 1 (Successful)
+ * Return: lenght of printed chararctes
  **/
 
 int print_octal(va_list list)
-{
-	int num = va_arg(list, int);
-	unsigned int *buffer_int;
-	int index = 0, i = 0;
-	int temp_num = num;
+{       /*the memory represent negative numbers as unsigned numbers*/
+	unsigned int num = va_arg(list, unsigned int);
+	char *buffer_int;
+	int index = 0, i = 0, len_nb = 0, len_printed = 0;
+	/*temp_num aux variable to get the lenght of number*/
+	unsigned int temp_num = num;
 
-	while (temp_num > 0)
-	{
-		temp_num /= 10;
-		i++;
-	}
-	buffer_int = (unsigned int *)malloc(sizeof(int) * i);
+	/*we verify if  one digit number  is passed*/
 	if (num < 8)
 	{
 		_putchar('0' + num);
 		return (1);
 	}
+	while (temp_num > 0)
+	{
+		temp_num /= 10;
+		len_nb++;
+	}
+	buffer_int = (char *)malloc(len_nb);
+	/*check if malloc failed*/
+	if (buffer_int == NULL)
+	{
+		return (-1);
+	}
+
 	while (num > 0)
 	{
-		buffer_int[index++] = num % 8;
+		buffer_int[index++] = (num % 8 + '0');
 		num /= 8;
 	}
 	for (i = index - 1; i >= 0; i--)
-		_putchar('0' + buffer_int[i]);
+	{
+		_putchar(buffer_int[i]);
+		len_printed++;
+	}
 	free(buffer_int);
-	return (1);
+	return (len_printed);
 }
