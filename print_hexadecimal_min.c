f6a1f3fae69afa09c8d60032b35b3bdb53f0a5ff
dev-tch <tijeni.chaabeni@gmail.com>
2023-07-31 00:11:53 +0000

    fix %x && %X

--- print_hexadecimal_min.c
+++ print_hexadecimal_min.c
@@ -4,42 +4,42 @@
  *			hexadecimal format(minuscule)
  * @list: argument list containing a character to be printed.
  *
- * Return: 1 (Successful)
+ * Return: number of printed characters
  **/
 
 int print_hexadecimal_min(va_list list)
 {
-	int num = va_arg(list, int);
-	unsigned int *buffer_int;
-	int index = 0, i = 0;
-	int temp_num = num;
-
-	while (temp_num > 0)
-	{
-		temp_num /= 10;
-		i++;
-	}
-	buffer_int = (unsigned int *)malloc(sizeof(int) * i);
-
-	if (num < 0)
-		return (-1);
+	unsigned int num = va_arg(list, unsigned int);
+	char  *buffer_int;
+	int index = 0, i = 0, len_nb = 0, len_printed = 0;
+	unsigned int temp_num = num;
+	const  char  items_of_bases[] = "0123456789abcdef";
+	/*we verify if one digit passed*/
 	if (num < 10)
 	{
 		_putchar('0' + num);
 		return (1);
 	}
+	/*we calculate the length of number*/
+	while (temp_num > 0)
+	{
+		temp_num /= 10;
+		len_nb++;
+	}
+	buffer_int = (char *)malloc(len_nb);
+	/*check if malloc failed*/
+	if (buffer_int == NULL)
+		return (-1);
 	while (num > 0)
 	{
-		buffer_int[index++] = num % 16;
+		buffer_int[index++] = items_of_bases[num % 16];
 		num /= 16;
 	}
 	for (i = index - 1; i >= 0; i--)
 	{
-		if (buffer_int[i] >= 10)
-			_putchar('a' + buffer_int[i] - 10);
-		else
-			_putchar('0' + buffer_int[i]);
+		_putchar(buffer_int[i]);
+		len_printed++;
 	}
 	free(buffer_int);
-	return (1);
+	return (len_printed);
 }
